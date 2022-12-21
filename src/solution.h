ba7e2eb102e8b0ca0c9d7d5c01ee3d81369bbc87
God Deam <martokonov@gmail.com>
2022-12-21 18:55:53 +0200

    implementation with errors

--- src/solution.h
+++ src/solution.h
@@ -1,5 +1,9 @@
 #ifndef WEEK11_HASH_H
+#include<string>
+#include <map>
 
+long long Hash(std::string str);
+long long RollingHash(std::string prev, long long H, char nxt);
 int rabinKarp(const char* s, const char* in);
 
 #endif //WEEK11_HASH_H
